0f13bfce7e94b5554022941f1375d8033c72cec7
gignac-cha <38489680+gignac-cha@users.noreply.github.com>
2019-02-19 21:35:25 +0900

    Update prepare.sh
    fix script

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install v11.10.0
 nvm use v11.10.0
