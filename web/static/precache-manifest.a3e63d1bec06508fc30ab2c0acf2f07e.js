f2c143726dc5d8da6cd165c457c5b23ec782aa99
Cha <arbeitcjh@gmail.com>
2019-05-13 14:13:47 +0900

    update package.json

---
+++ web/static/precache-manifest.a3e63d1bec06508fc30ab2c0acf2f07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c010ad2542e36269b64be738b495f9ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "284dcfd2a42864618b1f",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "284dcfd2a42864618b1f",
+    "url": "/static/js/main.be8d77ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
